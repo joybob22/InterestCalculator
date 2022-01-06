99c4216fb30767e18b361ee2c4ef07d269400abe
joybob22 <braydenlemke8@gmail.com>
2022-01-06 08:37:45 -0700

    initial commit

---
+++ InterestCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InterestCalculator
+//
+//  Created by Brayden Lemke on 1/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InterestCalculator/InterestCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterestCalculatorApp.swift
+//  InterestCalculator
+//
+//  Created by Brayden Lemke on 1/6/22.
+//
+
+import SwiftUI
+
+@main
+struct InterestCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
