d0814557185b7c3c8c8f3fa545ae4deec6a8cd07
joybob22 <braydenlemke8@gmail.com>
2022-01-12 08:55:41 -0700

    created delete functionality in list of loans view

--- InterestCalculator/LoanCalculator.swift
+++ InterestCalculator/LoanCalculator.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 
 
-struct ContentView: View {
+struct LoanCalculator: View {
     
     @State private var amount: Int? = nil
     @State private var interestRate: Double? = nil
@@ -70,6 +70,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LoanCalculator()
     }
 }
