6428ed570649b74e55c5ea52922748ca43652cd2
joybob22 <braydenlemke8@gmail.com>
2022-01-12 08:32:32 -0700

    started the delete

--- InterestCalculator/Controllers/LoanController.swift
+++ InterestCalculator/Controllers/LoanController.swift
@@ -8,6 +8,12 @@
 import Foundation
 
 struct LoanController {
+    static var testData: [Loan] = [
+        Loan(amount: 23000, interestRate: 0.07, term: 24, name: "Car Payment"),
+        Loan(amount: 450000, interestRate: 0.03, term: 360, name: "House Payment"),
+        Loan(amount: 77000, interestRate: 0.12, term: 50, name: "Tesla Payment")
+    ]
+    
     static func currencyFormatter(number: Double) -> String {
         let formatter = NumberFormatter()
         formatter.numberStyle = .currency
